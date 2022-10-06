a25cd82c8782e8bea4f1402d3e7ca1cf17cf80af
jjjxyce <114076686+jjjxyce@users.noreply.github.com>
2022-10-06 01:47:54 -0400

    Add files via upload

--- question_2/callback.js
+++ question_2/callback.js
@@ -1,1 +1,19 @@
+const resolvedPromise = () =>
+    new Promise ((resolve, reject) => {
+        setTimeout(resolve({message: "Delayed Success!" }), 500);
+
+    });
+
+
+const rejectPromise = () =>
+    new Promise((resolve, reject) => {
+        setTimeout(reject({error: "Delayed Exception!" }), 500);
+
+    });
+
+
+resolvedPromise().then((message) => console.log(message));
+rejectPromise().catch((message) => console.log(message));
+
+
 
