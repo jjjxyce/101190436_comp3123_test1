177ed3aa70abb85931d7ce7a2cdae2e9aaaacb64
jjjxyce <114076686+jjjxyce@users.noreply.github.com>
2022-10-06 01:43:20 -0400

    Add files via upload

--- question_1/index.js
+++ question_1/index.js
@@ -1,1 +1,24 @@
+function lowerCaseWords(array) {
+    result = [];
+    return new Promise((resolve, reject) => {
+        if(array.length == 0) {
+            reject(new Error("Invalid Array"))
+        } else {
+            for (item of array) {
+                if (typeof(item) == "string") {
+                    result.push(item.toLowerCase())
+                }
+            }
+            resolve(result)
+        }  
 
+    })
+}
+
+lowerCaseWords(["PIZZA", 10, true, 25, false, "Wings"])
+.then(result => console.log(result))
+.catch(error => console.log(error))
+
+lowerCaseWords([])
+.then(result => console.log(result))
+.catch(error => console.log(error))
