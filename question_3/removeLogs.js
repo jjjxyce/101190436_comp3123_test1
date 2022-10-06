8b26cd66620248d9a595ee5484f48af3b6ad4779
jjjxyce <114076686+jjjxyce@users.noreply.github.com>
2022-10-06 15:18:13 -0400

    Add files via upload

--- question_3/removeLogs.js
+++ question_3/removeLogs.js
@@ -1,1 +1,21 @@
+const fs = require("fs");
 
+const removeLogs = () =>{
+    const target = "./logs";
+    if (fs.existsSync(target)){
+        process.chdir(target);
+        fs.readdir(process.cwd(), (error, files) => {
+            files.forEach((file) => {
+                fs.unlink(file, (error) =>
+                error ? console.error (`Error Deleting a File '${file}'`) : console.log(`Deleting files...'${file}'`)
+                );
+
+            });
+
+        });
+
+    }
+
+};
+
+removeLogs();
