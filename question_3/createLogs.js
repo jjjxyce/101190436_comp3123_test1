4beb7445b2045bc0e0222a9348b8214e8aace399
jjjxyce <114076686+jjjxyce@users.noreply.github.com>
2022-10-06 15:17:15 -0400

    Add files via upload

--- question_3/createLogs.js
+++ question_3/createLogs.js
@@ -1,1 +1,19 @@
+const fs = require("fs");
 
+const createLogs = () => {
+    const target = "./logs";
+    if (!fs.existsSync(target)){
+        fs.mkdir(target);
+    }
+
+    process.chdir(target);
+    for (let i = 0; i < 10; i++){
+        fs.writeFile(`log${i}.txt`, `log file number ${i}`, (error) =>
+        error ? console.error(`log${i}.txt`) : console.log(`log${i}.txt`)
+
+        );
+    }
+
+};
+
+createLogs();
